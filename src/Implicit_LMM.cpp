20e67a4a1ea0cd89f63fadf0ae2c04a04ada6a57
M_axssi <290516875@qq.com>
2021-04-21 14:00:05 +0800

    a little changement

--- src/Implicit_LMM.cpp
+++ src/Implicit_LMM.cpp
@@ -4,11 +4,11 @@ void Implicit_LMM::Nonlinear_Solve(std::vector<Real> &  initial,std::vector<Real
 {
   int n=F.Get_n();
   Real * temp=VV_ADD(initial+C,K*F.Get_diff(initial));
-  //int count=0;
+  int count=0;
   Real error=cblas_dnrm2(n,temp,1);
-  while (error>1e-12)
+  while (error>1e-12 && count<=10)
     {
-      //++count;
+      ++count;
       Real * mat=F.Get_Jacobi(initial);
       for (int i=0;i<n;++i)
 	for (int j=0;j<n;++j)
